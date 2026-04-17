d08b4df5b4450c236206a2e7d9091c2eed3ca687
holos <holosnikitatt@gmail.com>
2026-04-17 19:53:47 +0300

    Update lib.cpp

--- kernel/lib.cpp
+++ kernel/lib.cpp
@@ -1,18 +1,42 @@
+#include <cstdint>
 #include "lib.hpp"
 volatile char* vimem=(volatile char*)0xB8000;
 int cursor=0;
+
+void outb(uint16_t port, uint8_t value){
+    asm volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
+}
+
+void updatecursor() {
+    uint16_t pos = cursor;
+
+    outb(0x3D4, 14);
+    outb(0x3D5, pos >> 8);
+
+    outb(0x3D4, 15);
+    outb(0x3D5, pos);
+}
 
 void print(const char* str) {
     while (*str){
         if (*str == '\n') {
             cursor+=(80 - (cursor % 80));
-        } else {
+        }
+        else if (*str == '\b') {
+            if (cursor > 0) {
+                cursor--;
+                vimem[cursor * 2] = ' ';
+                vimem[cursor * 2 + 1] = 0x07;
+            }
+        }
+        else {
             vimem[cursor * 2]=*str;
             vimem[cursor * 2 + 1]=0x07;
             cursor++;
         }
         str++;
     }
+    updatecursor();
 }
 
 void startlogo(){
